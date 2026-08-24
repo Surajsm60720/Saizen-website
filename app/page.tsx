4329e03ec0dfc3324c07f062391dac51cb508b83
Surajsm60720 <1ds22is166@dsce.edu.in>
2026-08-24 10:48:20 +0530

    feat: add footer, complete page assembly

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import { Pipeline } from '@/components/pipeline';
 import { Features } from '@/components/features';
 import { Security } from '@/components/security';
 import { Install } from '@/components/install';
+import { Footer } from '@/components/footer';
 
 export default function Home() {
   return (
-    <main id="top">
-      <Hero />
-      <Pipeline />
-      <Features />
-      <Security />
-      <Install />
-    </main>
+    <>
+      <main id="top">
+        <Hero />
+        <Pipeline />
+        <Features />
+        <Security />
+        <Install />
+      </main>
+      <Footer />
+    </>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <footer className="foot">
+      <div className="wrap">
+        <a href="https://github.com/Surajsm60720/Saizen">Source</a>
+        <a href="https://github.com/Surajsm60720/Saizen/releases">Releases</a>
+        <a href="https://github.com/Surajsm60720/Saizen/blob/main/docs/SECURITY_TEST_PLAN.md">Security</a>
+        <a href="https://github.com/Surajsm60720/Saizen/blob/main/STRUCTURE.md">Structure</a>
+        <p className="fine">
+          Personal sideload project. Do not redistribute copyrighted media. Respect local law and the terms of any
+          index you query.
+        </p>
+      </div>
+    </footer>
+  );
+}
